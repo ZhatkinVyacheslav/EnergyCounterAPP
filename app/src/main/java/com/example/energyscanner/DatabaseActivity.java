b30fcb65948c39c2c3b0a9ef7282c58951e9529e
mineshadow <shad-karagodin@ya.ru>
2023-12-20 18:18:37 +0300

    Fixed Date Formatting.

--- app/src/main/java/com/example/energyscanner/DatabaseActivity.java
+++ app/src/main/java/com/example/energyscanner/DatabaseActivity.java
@@ -1,5 +1,7 @@
 package com.example.energyscanner;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -10,6 +12,10 @@ import android.view.View;
 import android.widget.EditText;
 
 import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 
 public class DatabaseActivity extends AppCompatActivity {
     EditText editID;
@@ -39,14 +45,17 @@ public class DatabaseActivity extends AppCompatActivity {
 
     public void onFetchBtnClick(View v){
         Cursor cursor = dbManager.fetch();
-
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         if(cursor.moveToFirst()){
             do{
                 @SuppressLint("Range") String ID = cursor.getString(cursor.getColumnIndex(DatabaseHelper.CONTENT_ID));
                 @SuppressLint("Range") String Value = cursor.getString(cursor.getColumnIndex(DatabaseHelper.ENERGY_VAL));
                 @SuppressLint("Range") String date = cursor.getString(cursor.getColumnIndex(DatabaseHelper.ENERGY_DATE));
+                // Преобразуем значение даты в объект класса Date
+                Date dateFormatted = new Date(Long.parseLong(date));
+
                 Log.i("DATABASE_TAG", "I HAVE READ ID: " + ID + " ENERGY VALUE: " + Value
-                        + " DATE: " + date);
+                        + " DATE: " + dateFormatted);
             }while(cursor.moveToNext());
         }
     }
